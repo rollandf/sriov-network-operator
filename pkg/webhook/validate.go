077d03cf3795a542fff6184a4121eebce5d8d9c7
Ivan Kolodiazhnyi <ikolodiazhny@nvidia.com>
2026-07-30 12:45:39 +0300

    webhook: allow dash in resource name, add k8s naming reference
    Allow '-' in SriovNetworkNodePolicy resource names to align with
    Kubernetes naming conventions.
    
    Signed-off-by: Ivan Kolodiazhnyi <ikolodiazhny@nvidia.com>

--- pkg/webhook/validate.go
+++ pkg/webhook/validate.go
@@ -160,9 +160,9 @@ func validateSriovNetworkNodePolicy(cr *sriovnetworkv1.SriovNetworkNodePolicy, o
 }
 
 func staticValidateSriovNetworkNodePolicy(cr *sriovnetworkv1.SriovNetworkNodePolicy) (bool, error) {
-	var validString = regexp.MustCompile(`^[a-zA-Z0-9_]+$`)
+	var validString = regexp.MustCompile(`^[a-zA-Z0-9_-]+$`)
 	if !validString.MatchString(cr.Spec.ResourceName) {
-		return false, fmt.Errorf("resource name \"%s\" contains invalid characters, the accepted syntax of the regular expressions is: \"^[a-zA-Z0-9_]+$\"", cr.Spec.ResourceName)
+		return false, fmt.Errorf("resource name \"%s\" contains invalid characters, the accepted syntax of the regular expressions is: \"^[a-zA-Z0-9_-]+$\", see https://kubernetes.io/docs/concepts/overview/working-with-objects/names/#names", cr.Spec.ResourceName)
 	}
 
 	if cr.Spec.NicSelector.Vendor == "" && cr.Spec.NicSelector.DeviceID == "" && len(cr.Spec.NicSelector.PfNames) == 0 && len(cr.Spec.NicSelector.RootDevices) == 0 && cr.Spec.NicSelector.NetFilter == "" {
